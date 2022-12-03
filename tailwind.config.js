b014326859a7325e657ad4eb2d14375eeb96566b
Diego <110729317+dgo-gco@users.noreply.github.com>
2022-12-03 18:23:30 +0100

    adding files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+      extend: {
+        colors: {
+          'dark-blue': 'hsl(233, 47%, 7%)',
+          'des-blue': 'hsl(244, 38%, 16%)',
+          'soft-violet': 'hsl(277, 64%, 61%)',
+          'white-head': 'hsl(0, 0%, 100%)',
+          'slight-white': 'hsla(0, 0%, 100%, 0.75)',
+          'dslight-white': 'hsla(0, 0%, 100%, 0.6)'
+        }
+      },
+    },
+    plugins: [],
+  }
+
